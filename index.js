ba7030146f0af2ba444aacfc0cc7ada67e597582
LorenzoASC23 <139983093+LorenzoASC23@users.noreply.github.com>
2025-10-21 19:06:38 +0000

    done

--- index.js
+++ index.js
@@ -7,6 +7,12 @@
 //    - Convert the response to JSON
 //    - Log out the JSON or specific info (like name or country)
 
+async function fetchUserData(url) {
+const response = await fetch(url)
+const data = await response.json()
+console.log(data)
+return data   
+}
 
 
 // ----------------------------------------------------
@@ -15,32 +21,34 @@
 
 // 1 URL to Get 8 Random Users
 // Call the fetchUserData function with this URL
-
-
+let url = "https://randomuser.me/api/?results=8"
+fetchUserData(url)
 
 // 2 URL to Get a Random User from Poland
 // Call the fetchUserData function with this URL
-
-
+let url2 = "https://randomuser.me/api/?nat=FR"
+fetchUserData(url2)
 
 
 
 // 3 URL to Get a Random User that’s Male
 // Call the fetchUserData function with this URL
-
+let url3 = "https://randomuser.me/api/?gender=female"
+fetchUserData(url3)
 
 
 
 // 4 URL to Get a Random User while Excluding Their Email
 // Call the fetchUserData function with this URL
-
-
+let url4 = "https://randomuser.me/api/?exc=login"
+fetchUserData(url4)
 
 
 
 // 5 Get 5 Female Users from the Colombia
 // Call the fetchUserData function with this URL
 
-
+let url5 = "https://randomuser.me/api/?results=5&nat=CA&gender=female"
+fetchUserData(url5)
 
 
